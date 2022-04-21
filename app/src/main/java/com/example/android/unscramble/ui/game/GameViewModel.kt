bbf4447fdcf11ae8c00cbea5a939bbd50c695c3d
gwabstech <72021618+gwabstech@users.noreply.github.com>
2022-04-21 13:17:31 +0100

    done

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,24 +1,32 @@
-package com.example.android.unscramble.ui.game
-
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.ui.game.MAX_NO_OF_WORDS
+import com.example.android.unscramble.ui.game.SCORE_INCREASE
+import com.example.android.unscramble.ui.game.allWordsList
 
+/**
+ * ViewModel containing the app data and methods to process the data
+ */
 class GameViewModel : ViewModel(){
+    private var _score = 0
+    val score: Int
+        get() = _score
 
-    public var score = 0
     private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
     private lateinit var _currentScrambledWord: String
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
-
-
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+    // List of words used in the game
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     init {
-        getNextWord()
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
     override fun onCleared() {
@@ -26,9 +34,14 @@ class GameViewModel : ViewModel(){
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
 
+    /*
+    * Updates currentWord and currentScrambledWord with the next word.
+    */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
@@ -41,16 +54,43 @@ class GameViewModel : ViewModel(){
         }
     }
 
-    fun nextWord(): Boolean{
-        return if (_currentWordCount< MAX_NO_OF_WORDS){
-            getNextWord()
-            true
-        }else false
+    /*
+    * Re-initializes the game data to restart the game.
+    */
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
     }
 
 
-    private var _score = 0
-    val scoree: Int
-        get() = _score
+    /*
+    * Increases the game score if the player's word is correct.
+    */
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
 
+    /*
+    * Returns true if the player word is correct.
+    * Increases the score accordingly.
+    */
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS
+    */
+    fun nextWord(): Boolean {
+        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 }
